b42f5542a11beb7f993a826d521fa24b663b04b5
Matt Edman <edmanm@vidalia-project.net>
2009-06-01 07:03:24 +0000

    And backport the r3809 half of r3810 to 0.1.x.
    svn:r3811

--- src/common/win32.cpp
+++ src/common/win32.cpp
@@ -153,7 +153,8 @@ win32_registry_remove_key(QString keyLocation, QString keyName)
 /**
  * Callback for EnumThreadWindows which sends the WM_QUIT message
  */
-BOOL CALLBACK quitWindowCallback(HWND hwnd, LPARAM targetPID)
+BOOL CALLBACK 
+quitWindowCallback(HWND hwnd, LPARAM targetPID)
 {
   DWORD hwndPID = 0;
 
@@ -162,6 +163,7 @@ BOOL CALLBACK quitWindowCallback(HWND hwnd, LPARAM targetPID)
   GetWindowThreadProcessId(hwnd, &hwndPID);
   if (hwndPID == (DWORD)targetPID)
     PostMessage(hwnd, WM_QUIT, 0, (LPARAM)NULL);
+  return TRUE;
 }
 
 /**
